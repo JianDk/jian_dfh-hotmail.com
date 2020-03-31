4d792659b622d7689c3f133a5d7161cde1ac7244
unknown <jian_dfh@hotmail.com>
2020-03-31 20:16:05 +0200

    back up

--- deletem4.py
+++ deletem4.py
@@ -1,4 +1,29 @@
-import json
+import imaplib 
+import email
+import sys
 
-with open('Shopify_item2print.txt') as f:
-  translation = json.load(f)
+#Clean up in mail box
+email = 'kontakt@dimsum.dk'
+host = 'imap.gigahost.dk'
+password = 'DimSum2018'
+port = 993
+
+connection = imaplib.IMAP4_SSL(host = host, port = port)
+connection.login(user = email, password = password)
+connection.select('INBOX')
+
+#Perform a filter to search for emails from secomapp
+result, mailid_from = connection.search(None, f'(FROM "notifications-noreply@secomapp.com")')
+
+if result != 'OK':
+    sys.exit("No emails from Secomapp stop")
+
+for mailid in mailid_from:
+    print(mailid)
+    connection.close()
+    connection.logout()
+
+
+#Close the connection
+connection.close()
+connection.logout()
--- deleteme.py
+++
@@ -1,4 +0,0 @@
-translation = dict()
-translation['jian'] = 'Mai'
-if 'jian' in translation:
-    print('yes')
