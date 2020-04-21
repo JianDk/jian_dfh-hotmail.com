d629528281a908c151d7a4540a5f0aa5581bbd0a
JianDk <jian_dfh@hotmail.com>
2020-04-21 10:19:33 +0200

    working on bracnch

--- ShopifyManagement.py
+++ ShopifyManagement.py
@@ -1,8 +1,9 @@
 import datetime
+import logging
+import requests
+
 class ManageOrder:
-
     def __init__(self, **kwargs):
-
         self.API_VERSION = "2020-04"
 
         if kwargs['switch'] == 'DK':
@@ -16,20 +17,75 @@ class ManageOrder:
             self.PASSWORD = "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"
             self.shop_url = "https://%s:%s@alexanderystore.myshopify.com/admin/api/%s" % (self.API_KEY, self.PASSWORD, self.API_VERSION)
             self.databasePath = 'orderDBHK.db'
+        
+        #DEfine conversion rate from HKD to DKK
+        self.conversion_HKD_DKK = 0.89
     
-    def incomeStatus(self):
+    def incomeStatus(self, switch):
         '''
         get how much income is currently in HK and DK site for the current month. The currency is in DK.
         '''
         #Get current income in HK for this month
-        self.__init__(switch = 'HK')
+        self.__init__(switch = switch)
         currentMonth = datetime.datetime.now().month
         #Get the payouts from HK for this month
-        
-        print(currentMonth)
-        
+        payout_url = self.shop_url  + "/shopify_payments/payouts.json"
+        resp = requests.get(url = payout_url)
+        if resp.status_code != 200:
+            status = False
+            amount = False
+            #Need to create a log file here
+            return status, amount
 
+        resp = resp.json()['payouts']
+        month_income = list()
+        for item in resp:
+            #Check if payout date is within current date
+            date = datetime.datetime.strptime(item['date'], "%Y-%m-%d")
+            if date.month == currentMonth and item['status'] != 'failed':
+                #The income for this transaction will be counted
+                
+                #Get currency format
+                if item['currency'] == 'HKD':
+                    month_income.append(float(item['amount']) * self.conversion_HKD_DKK)
+                elif item['currency'] == 'DKK':
+                    month_income.append(float(item['amount']))
+
+        amount = round(sum(month_income))
+        status = True
+        return status, amount
+    
+    def account_switch_decision(self, maxIncome):
+        '''
+        Check if amount in DK account and decide if account should be changed
+        '''
+        status, amount = self.incomeStatus(switch = 'DK')
+        if status == False:
+            status = False
+            return status
+        #try to log some text
+        self.logging('debug', 'hello world from logging another time')
+    
+    def logging(self, level, message):
+        print('called')
+        #Instantiate logging
+        logging.basicConfig(filename = 'log.txt', filemode = 'w', level = logging.DEBUG)
+        if level == 'debug':
+            logging.debug(message)
+        
+        if level == 'info':
+            logging.info(message)
+        
+        if level == 'warning':
+            logging.warning(message)
+
+        if level == 'error':
+            logging.error(message)
+        
+        if level == 'critical':
+            logging.critical(message)
+            
 
 mo = ManageOrder(switch = 'HK')
-mo.incomeStatus()
+status, amount = mo.incomeStatus(switch = 'DK')
 
